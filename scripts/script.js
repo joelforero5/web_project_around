31a13aa04ba51baaa9b035733e68aaf85158cda9
joelforero5 <joelforero5@gmail.com>
2025-06-12 17:14:41 -0400

    -Creacion de ventana modal, de acuerdo al diseño -aun no se establece nada responsive -Creacion de metodos en javascript para apertura y cierre de la ventana -Se añaden hover a los elementos correspondientes.

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const editButton = document.querySelector(".profile__button_edit");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__button-cancel");
+debugger;
+editButton.addEventListener("click", () => {
+  popup.classList.add("popup__opened");
+});
+
+closeButton.addEventListener("click", () => {
+  popup.classList.remove("popup__opened");
+});
